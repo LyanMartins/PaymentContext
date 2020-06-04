ae2ee3999b639c510418855997efa77577287834
Lyan Martins <lyan.martins3@gmail.com>
2020-06-04 20:54:25 -0300

    Added command for credicard, paypal and boleto payment

---
+++ PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObject;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class CreateBoletoSubscriptionCommandTests
+    {
+        //[TestMethod]
+       
+    }
+}
